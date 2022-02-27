0aae1c68e63458452213ea8164e3a19347d9e86a
Алексей <burdykin@live.ru>
2022-02-27 16:36:00 +0300

    кеширование одной картинки

--- lab/cachApi/app.js
+++ lab/cachApi/app.js
@@ -1,12 +1,15 @@
 ﻿// подключаем кнопки
 window.addEventListener("DOMContentLoaded", async event => {
     document.querySelector("#btNet").addEventListener("click", fromNet);
-    document.querySelector("#btCache").addEventListener("click", fromCache);
+    //document.querySelector("#btCache").addEventListener("click", fromCache);
    /* document.querySelector("#delete").addEventListener("click", deleteCache);*/
 });
 
-const DOG_API ="https://dog.ceo/api/breeds/image/random"; //анонимное api случайных картинок собак. Для тестирования
+const DOG_API = "https://dog.ceo/api/breeds/image/random"; //анонимное api случайных картинок собак. Для тестирования
+const CACHE_NAME = "dog.ceo/api/breeds/image/random";
 
+/* Прочитать данные из внешнего Api 
+ */
 async function fromNet() {
     //Использование Fetch Api https://developer.mozilla.org/ru/docs/Web/API/Fetch_API/Using_Fetch
 
@@ -27,8 +30,7 @@ async function fromNet() {
            
         })
         .then((data) => { //затем обрабатываем полученные json данные
-            document.getElementById("dogImg").src = data.message; //получаем только имя картинки, сама картинка грузится браузером автоматически, при изменеии src
-            showResult("Новая картинка: " + data.message);
+            DataProcessing(data.message);
         })
         .catch((e) => {
             showResult("Ошибка запроса: " + e.message);
@@ -36,6 +38,53 @@ async function fromNet() {
         });
 }
 
+
+function DataProcessing(data) {
+       //отобразить картинку
+    var dogImg = document.getElementById("dogImg").src = data; //получаем только имя картинки, сама картинка грузится браузером автоматически, при изменеии src
+
+    deleteCache(CACHE_NAME); //полное удаление кеша, чтобы картинки не крпились до бесконечности)
+    cacheSingleImage(data); //добавляем картинку в кеш
+    //showResult("Новая картинка: " + data);
+}
+
+async function deleteCache(cacheName) {
+    if ('caches' in window) {
+        try {
+            caches.delete(cacheName).then(function (ex) { // https://developer.mozilla.org/en-US/docs/Web/API/CacheStorage/delete
+                if (ex) console.log("кеш " + cacheName + " удален");
+                else console.log("кеш " + cacheName + " не существует");
+            });
+
+            //await caches.delete(cacheName); //caches -- удаляет весь кеш по имени. Метод CacheStorage
+            //полная очистка всех кешей https://developer.mozilla.org/en-US/docs/Web/API/CacheStorage/delete
+
+           
+        } catch (error) {
+            console.log("Ошибка при удалении кеша" + cacheName + " ==> " + error.message);
+    }
+    } else {
+        showResult("Хранилище кеша не поддерживается.");
+    }
+}
+
+
+//Добавляем картинку в кеш, без обращения к сети
+async function cacheSingleImage(imgUrl) {
+    if ('caches' in window) {
+        try {
+            const cache = await caches.open(CACHE_NAME);
+            await cache.add(imgUrl);
+            showResult("файл " + imgUrl + "кеширован в " + cacheName);
+        } catch (error) {
+            showResult("Ошибка кеширования " + error.message);
+        }
+    }  else {
+        showResult("Хранилище кеша не поддерживается.");
+    }
+}
+
 function showResult(text) {
     document.querySelector("output").innerHTML = text;
 }
+
