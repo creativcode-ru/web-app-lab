7acbdded53964b4e33a73ecac708cddfd6444ae7
Алексей <burdykin@live.ru>
2022-03-04 19:37:39 +0300

    simple web worker

---
+++ WebApi/WebWorkers/wwSimple/wwSimple.js
@@ -0,0 +1,11 @@
+﻿onmessage = function (e) {
+    console.log('Worker: Получено сообщение из main.js');
+    const result = e.data[0] * e.data[1];
+    if (isNaN(result)) {
+        postMessage('Введите два номера');
+    } else {
+        const workerResult = 'Результат: ' + result;
+        console.log('Worker: Отправить сообщение обратно в main.js');
+        postMessage(workerResult);
+    }
+}
